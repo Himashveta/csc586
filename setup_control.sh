578039d086de667385781602473126a2bd27f390
Himashveta Kumar <hk979356@wcupa.edu>
2023-06-22 17:41:08 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # generate passwordless SSH
-runuser -u Shveta -- ssh-keygen -q -t rsa -f /users/Shveta/.ssh/id_rsa -N ''
-runuser -u Shveta -- cat /users/Shveta/.ssh/id_rsa.pub >> /users/Shveta/.ssh/authorized_keys
+runuser -u lngo -- ssh-keygen -q -t rsa -f /users/lngo/.ssh/id_rsa -N ''
+runuser -u lngo -- cat /users/lngo/.ssh/id_rsa.pub >> /users/lngo/.ssh/authorized_keys
 
 # setup NFS for key sharing
 apt update
 apt install -y nfs-kernel-server
 mkdir /var/nfs/keys -p
-cp /users/Shveta/.ssh/id_rsa* /var/nfs/keys
+cp /users/lngo/.ssh/id_rsa* /var/nfs/keys
 chown nobody:nogroup /var/nfs/keys
 echo "/var/nfs/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 systemctl restart nfs-kernel-server
@@ -19,11 +19,10 @@ apt update
 apt install -y ansible
 cp /local/repository/hosts /etc/ansible/
 
+# configure to disable strict hostkey checking
+echo "[defaults]" | sudo tee -a /etc/ansible/ansible.cfg
+echo "host_key_checking = False" | sudo tee -a /etc/ansible/ansible.cfg
+
 # setup lamp
-git clone https://github.com/do-community/ansible-playbooks.git
-cd ansible-playbooks/lamp_ubuntu1804/
-cp /local/repository/default.yml vars/
-HOST=$(hostname -f)
-sed -i "s/HOSTNAME/$HOST/g" vars/default.yml
-ansible-playbook playbook.yml -l server1 -u Shveta
+runuser -u lngo -- bash /local/repository/setup_ansible.sh
 
