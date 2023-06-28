9be6e9bbf465250041ad52752cc6e292b5fde3d7
Himashveta Kumar <hk979356@wcupa.edu>
2023-06-28 11:15:18 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,4 +1,3 @@
-
 import geni.portal as portal
 import geni.rspec.pg as rspec
          
@@ -8,27 +7,24 @@ request = portal.context.makeRequestRSpec()
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-# Create a XenVM
-for i in range(2):
-  if i == 0:
-    node = request.XenVM("webserver")
-    node.routable_control_ip = "true"
-  elif i == 1: 
-    node = request.XenVM("observer")
-    node.routable_control_ip = "false"
-  else:
-    node = request.XenVM("ldap")
-    node.routable_control_ip = "false"
+# Create XenVMs
+for i in range(3):
+    if i == 0:
+        node = request.XenVM("webserver")
+        node.routable_control_ip = "true"
+        node.addService(rspec.Execute(shell="sh", command="sudo apt-get update"))
+        node.addService(rspec.Execute(shell="sh", command="sudo apt-get install -y apache2"))
+    else: 
+        node = request.XenVM("observer")
+        node.routable_control_ip = "false"
+        node.addService(rspec.Execute(shell="sh", command="sudo apt-get update"))
+        node.addService(rspec.Execute(shell="sh", command="sudo apt-get install -y nfs-kernel-server"))
 
-  
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
-  #if i == 0:
-  #  node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
-    
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+    iface = node.addInterface("if" + str(i))
+    iface.component_id = "eth1"
+    iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+    link.addInterface(iface)
+
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
